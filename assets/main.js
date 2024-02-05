9a2154e0837e5fde6e586ce952e119c72483bf3a
balqeesomar <62262971+balqeesomar@users.noreply.github.com>
2024-02-05 21:53:41 +0200

    task9

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("sidebar").style.width = "250px";
+  document.getElementById("overlay").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("overlay").style.width = "0";
+}
+
